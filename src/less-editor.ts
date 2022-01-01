dc949c21ae9c6089d44346d3302b75d59fac63fc
NEOTSO <neo.tso@outlook.com>
2022-01-01 09:38:54 +0800

    feat: isCollasped mode, tabindent reverse

--- src/less-editor.ts
+++ src/less-editor.ts
@@ -15,42 +15,56 @@ export class LessEditor {
     }
 
     init() {
-        this.tabIndent()
-        this.tabIndentReverse()
+        this.listenIndent()
     }
 
-    tabIndent() {
-        const tabSpace = new Array(this.tabWidth).fill('\u00A0').join('')
+    listenIndent() {
         this.el?.addEventListener('keydown', e => {
             if (e.key === 'Tab') {
                 e.preventDefault()
-                // console.log(this.el?.children)
-                // console.log(this.el?.childNodes)
-                // console.log(this.sel?.anchorOffset)
-                // console.log(this.sel?.rangeCount)
-                const range = this.sel?.getRangeAt(0)
-                if (this.sel?.isCollapsed) {
-                    const anchorOffset = this.sel?.anchorOffset
-                    const anchorNode = this.sel?.anchorNode
-                    const lineNum = this.getLineNum(this.el?.childNodes as NodeList, anchorNode as Text)
-                    if (anchorNode?.nodeType === Node.ELEMENT_NODE) {
-                        const textNode = document.createTextNode(tabSpace)
-                        console.log(anchorNode.childNodes)
-                        anchorNode.replaceChild(textNode, anchorNode.childNodes[0])
-                        range.setStart(textNode, this.tabWidth)
-                        range.setEnd(textNode, this.tabWidth)
-                    } else if (anchorNode?.nodeType === Node.TEXT_NODE) {
-                        anchorNode?.insertData(anchorOffset, tabSpace)
-                        range.setStart(anchorNode as Node, anchorOffset + this.tabWidth)
-                        range.setEnd(anchorNode as Node, anchorOffset + this.tabWidth)
-                    }
+                if (e.shiftKey) {
+                    this.tabIndentReverse()
+                } else {
+                    this.tabIndent()
                 }
+
             }
         })
     }
 
-    tabIndentReverse() {
+    tabIndent() {
+        const tabSpace = new Array(this.tabWidth).fill('\u00A0').join('')
+        const range = this.sel?.getRangeAt(0)
+        if (this.sel?.isCollapsed) {
+            const anchorOffset = this.sel?.anchorOffset
+            const anchorNode = this.sel?.anchorNode
+            const lineNum = this.getLineNum(this.el?.childNodes as NodeList, anchorNode as Text)
+            if (anchorNode?.nodeType === Node.ELEMENT_NODE) {
+                const textNode = document.createTextNode(tabSpace)
+                console.log(anchorNode.childNodes)
+                anchorNode.replaceChild(textNode, anchorNode.childNodes[0])
+                range.setStart(textNode, this.tabWidth)
+                range.setEnd(textNode, this.tabWidth)
+            } else if (anchorNode?.nodeType === Node.TEXT_NODE) {
+                anchorNode?.insertData(anchorOffset, tabSpace)
+                range.setStart(anchorNode as Node, anchorOffset + this.tabWidth)
+                range.setEnd(anchorNode as Node, anchorOffset + this.tabWidth)
+            }
+        }
+    }
 
+    tabIndentReverse() {
+        const tabSpace = new Array(this.tabWidth).fill('\u00A0').join('')
+        if (this.sel?.isCollapsed) {
+            const anchorOffset = this.sel?.anchorOffset
+            const anchorNode = this.sel?.anchorNode
+            if (anchorNode?.nodeType === Node.TEXT_NODE) {
+                const result = anchorNode.nodeValue?.match(/^\s{1,4}/)
+                if (result) {
+                    anchorNode.deleteData(0, result[0].length)
+                }
+            }
+        }
     }
 
     getLineNum(nodes: NodeList, node: Text) {
