5443850337ac84a8d3e7e0f7a0b246217703bfe5
NEOTSO <neo.tso@outlook.com>
2022-01-02 11:23:48 +0800

    feat: select multiple line text, tab indent

--- src/less-editor.ts
+++ src/less-editor.ts
@@ -6,11 +6,13 @@ export interface EditorConfig {
 export class LessEditor {
     el: HTMLElement | null = null
     tabWidth: number = 4
+    tabSpace: string = new Array(4).fill('\u00A0').join('')
     sel: Selection | null = window.getSelection()
     constructor(config: EditorConfig) {
         const { id, tabWidth } = config
         this.el = document.getElementById(id)
         this.tabWidth = tabWidth
+        this.tabSpace = new Array(tabWidth).fill('\u00A0').join('')
         this.init()
     }
 
@@ -33,37 +35,52 @@ export class LessEditor {
     }
 
     tabIndent() {
-        const tabSpace = new Array(this.tabWidth).fill('\u00A0').join('')
         const range = this.sel?.getRangeAt(0)
         if (this.sel?.isCollapsed) {
             const anchorOffset = this.sel?.anchorOffset
             const anchorNode = this.sel?.anchorNode
             const lineNum = this.getLineNum(this.el?.childNodes as NodeList, anchorNode as Text)
             if (anchorNode?.nodeType === Node.ELEMENT_NODE) {
-                const textNode = document.createTextNode(tabSpace)
+                const textNode = document.createTextNode(this.tabSpace)
                 console.log(anchorNode.childNodes)
                 anchorNode.replaceChild(textNode, anchorNode.childNodes[0])
                 range.setStart(textNode, this.tabWidth)
                 range.setEnd(textNode, this.tabWidth)
             } else if (anchorNode?.nodeType === Node.TEXT_NODE) {
-                anchorNode?.insertData(anchorOffset, tabSpace)
+                anchorNode?.insertData(anchorOffset, this.tabSpace)
                 range.setStart(anchorNode as Node, anchorOffset + this.tabWidth)
                 range.setEnd(anchorNode as Node, anchorOffset + this.tabWidth)
             }
         } else {
             const anchorOffset = this.sel?.anchorOffset
             const anchorNode = this.sel?.anchorNode
-            console.log(anchorNode)
-            anchorNode?.insertData(0, tabSpace);
+            const focusNode = this.sel?.focusNode
+            this.el?.childNodes.forEach(item => {
+                if (this.sel?.containsNode(item)) {
+                    if (item !== anchorNode && item !== focusNode) {
+                        this.indentNode(item)
+                    }
+                }
+            })
+            this.indentNode(anchorNode as Node)
+            this.indentNode(focusNode as Node)
+        }
+    }
+
+    indentNode(node: Node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            node.insertData(0, this.tabSpace)
+        } else if (node.nodeType === Node.ELEMENT_NODE) {
+            if (node.childNodes[0].nodeType === Node.TEXT_NODE) {
+                this.indentNode(node.childNodes[0])
+            } else {
+                const textNode = document.createTextNode(this.tabSpace)
+                node.replaceChild(textNode, node.childNodes[0])
+            }
         }
     }
 
     tabIndentReverse() {
-        const tabSpace = new Array(this.tabWidth).fill('\u00A0').join('')
-        // if (this.sel?.isCollapsed) {
-
-        // }
-
         const anchorOffset = this.sel?.anchorOffset
         const anchorNode = this.sel?.anchorNode
         if (anchorNode?.nodeType === Node.TEXT_NODE) {
